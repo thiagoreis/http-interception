1b6d7abd77765fe10e7b2b9469f282c5e0694dc4
Thiago Reis <thiagoreis128@gmail.com>
2018-02-09 13:39:44 -0200

    Capturando erro nas respostas

--- src/app/services/my-custom-interceptor.service.ts
+++ src/app/services/my-custom-interceptor.service.ts
@@ -1,12 +1,16 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
 import {
   HttpInterceptor,
   HttpRequest,
   HttpHandler,
   HttpEvent
-} from "@angular/common/http";
-import { Observable } from "rxjs/Observable";
-
+} from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+import { HttpResponse } from 'selenium-webdriver/http';
+import { HttpErrorResponse } from '@angular/common/http/src/response';
 @Injectable()
 export class MyCustomInterceptorService implements HttpInterceptor {
   intercept(
@@ -16,6 +20,17 @@ export class MyCustomInterceptorService implements HttpInterceptor {
     const cloneReq = request.clone({
       headers: request.headers.set('Authorization', 'Bearer token')
     });
-    return next.handle(cloneReq);
+    return next.handle(cloneReq)
+      .do((evento: HttpEvent<any>) => {
+        if (evento instanceof HttpResponse) {
+          console.log(evento);
+        }
+      }).catch(response => {
+        if (response instanceof HttpErrorResponse) {
+          console.log(response);
+        }
+
+        return Observable.throw(response);
+      });
   }
 }
